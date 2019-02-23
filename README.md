c272b67dc6f3210d92c0eb3cedf38bd835faa13b
edthomas93 <et1270@my.bristol.ac.uk>
2019-02-23 14:15:28 +0000

    Set up server

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # chitter-express
+
+## How to use
+
+1. clone the repository `git clone git@github.com:edthomas93/chitter-express.git`
+
+2. run `npm install`
+
+3. run `npx nodemon`
---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log('listening on port ' + port);
+})
+
+app.get('/', (request, response) => {
+  console.log(__dirname)
+  response.send('Hello world')
+})
